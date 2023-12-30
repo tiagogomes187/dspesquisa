761ce07aa0b6c0385f6f08331c7e9fb35773a531
Tiago Gomes <tiagogomes187@gmail.com>
2023-12-30 14:31:24 -0300

    Project Created

---
+++ backend/src/test/java/br/dev/tiagogomes/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.tiagogomes.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
